e3acfa1f59a4a8e3c42155c39737cd881c811d66
Andrés Puello <andrespuello53@gmail.com>
2023-05-07 01:01:39 -0500

    add other data

--- src/domain/project.js
+++ src/domain/project.js
@@ -5,6 +5,12 @@ const projects = [
     longitude: "-75.508454",
     description: "Calle de Piedra de bolivar",
   },
+  {
+    id: 2,
+    latitude: "10.401947",
+    longitude: "-75.515299",
+    description: "Una calle del barrio",
+  },
 ];
 
 module.exports = projects;
